9ba784f9400246a22c9e792ff76f26e5de2b855e
dienos <dienos2@gmail.com>
2026-02-24 13:41:59 +0900

    feat: Implement Android home widget functionality to navigate to daily log details or add screen

--- android/app/src/main/kotlin/com/dienos/dienos_calendar/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dienos.dienos_calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/e/dienos_calendar/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dienos.calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
